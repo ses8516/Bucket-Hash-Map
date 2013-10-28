1f5e0e3742782f72aa2feabb48fe6ed1c3d1960c
Stefano <ses8516@rit.edu>
2013-10-27 23:56:44 -0400

    test class done

--- Bucket-Hash-Map/src/Test.java
+++ Bucket-Hash-Map/src/Test.java
@@ -1,20 +1,29 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class Test extends Thread{
 
-	private static final int PER_THREAD = 5;
-	private static final int NUM_THREADS = 5;
+	private static final int PER_THREAD = 10;
+	private static final int NUM_THREADS = 10;
+	private static final int NUM_BUCKETS = 5;
 	
-	private static ConcurrentBucketHashMap<Integer , String> MAP = new ConcurrentBucketHashMap<Integer , String>(5);
+	private static ConcurrentBucketHashMap<Integer , String> MAP = new ConcurrentBucketHashMap<Integer , String>(NUM_BUCKETS);
 	
-	private static volatile int ELEMENT = 1;
+	private static volatile int KEY = 1;
 	
 	public Test(String name) {
 		super(name);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			System.setOut(new PrintStream("Trace.txt"));
+		} catch (FileNotFoundException e) {
+			System.err.println("Trace.txt file not found.");
+			return;
+		}
+		
 		ArrayList<Thread> threads = new ArrayList<Thread>(NUM_THREADS);
 		
 		for(int i = 0; i < NUM_THREADS; i++){
@@ -29,9 +38,35 @@ public class Test extends Thread{
 	public void run(){
 		// Add Elements to bucket
 		for(int i = 0; i < PER_THREAD; i++){
-			MAP.put(ELEMENT, ""+ELEMENT);
+			MAP.put(KEY, ""+KEY);
+			KEY++;
 		}
 		
-		
+		while(true){
+			int option = (int) (Math.random() * 5);
+			
+			if(option == 0){
+				MAP.put(KEY, ""+KEY);
+				KEY++;
+			}
+			else if(option == 1){
+				MAP.containsKey((int) (Math.random() * KEY));
+			}
+			else if(option == 2){
+				MAP.size();
+			}
+			else if(option == 3){
+				MAP.get((int) (Math.random() * KEY));
+			}
+			else if(option == 4){
+				MAP.remove((int) (Math.random() * KEY));
+			}
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
